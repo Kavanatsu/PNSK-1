e2bc366a5db9469818557790a100d6995a101e7e
Kavanatsu <20062000@inbox.ru>
2023-03-06 16:55:44 +0700

    Practice 7 not ended!!

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of socks",
         link:"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -13,11 +13,15 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+                onSale: true
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
+                onSale: false
             }
          ],
          sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -30,9 +34,24 @@ let app = new Vue({
         removeFromCart () {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
          } 
-     }     
+     }, 
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+         },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            return this.brand + ' ' + this.product + this.variants[this.selectedVariant].onSale;
+        }
+    }      
  })
  
